ea860a189877dbfa97aefa94b366dbe9ea8a705f
enma-pd <enma@perez.es>
2025-07-21 23:13:47 +0200

    update, create y delete

--- src/test/java/es/cic/curso25/proy008/Service/LibroServiceTest.java
+++ src/test/java/es/cic/curso25/proy008/Service/LibroServiceTest.java
@@ -33,12 +33,9 @@ public class LibroServiceTest {
         libro.setAutor("Federico García Lorca");
         libro.setAñoDePublicacion(1928);
         libro.setNombreLibro("Romancero Gitano");
-        libroService.create(libro).setId(1l);
-        libroService.delete(1);
-        assertNull(libro);
-
-
-
+        Libro libro1 = libroService.create(libro);
+        libroService.delete(libro1.getId());
+        assertNull(libro1);
     }
 
     @Test
