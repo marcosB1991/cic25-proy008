1ab62061d253405af3bd7b8f3e0845bc66eb34e4
enma-pd <enma@perez.es>
2025-07-21 23:04:16 +0200

    voy a mergear para hacer una prueba

--- src/test/java/es/cic/curso25/proy008/Service/LibroServiceTest.java
+++ src/test/java/es/cic/curso25/proy008/Service/LibroServiceTest.java
@@ -18,13 +18,11 @@ public class LibroServiceTest {
     @Test
     void testCreate() {
         Libro libro = new Libro();
-        Long id= new Long(1);
-        libro.setId(id);
         libro.setAutor("Federico García Lorca");
         libro.setAñoDePublicacion(1928);
         libro.setNombreLibro("Romancero Gitano");
         Libro libro1 = libroService.create(libro);
-        assertInstanceOf(Libro.class, libro1);
+        assertTrue(libro1.getId()==1);
     }
 
     
@@ -35,8 +33,7 @@ public class LibroServiceTest {
         libro.setAutor("Federico García Lorca");
         libro.setAñoDePublicacion(1928);
         libro.setNombreLibro("Romancero Gitano");
-        Long id = new Long(1);
-        libroService.create(libro).setId(id);
+        libroService.create(libro).setId(1l);
         libroService.delete(1);
         assertNull(libro);
 
